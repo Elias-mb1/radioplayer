6b1dd7b827f5fcb2df5889fcd86572a8b86405c7
Elias-mb1 <elias.burhan@chasacademy.se>
2023-12-02 19:02:47 +0100

    style radio

--- testar.js
+++ testar.js
@@ -15,13 +15,13 @@ fetch("https://api.sr.se/api/v2/channels/?format=json")
       const channelElement = document.createElement("div");
       channelElement.classList.add("channel");
       channelElement.innerHTML = `
-            <div style="display:flex; display:inline-block; flex-direction:row;">
-                <div >
-                    <h2>${channel.name}</h2>
+            <div style="display:flex; background-color:${changeColor(channel.name)}" class="parentWidth border" >
+                <div class=" flex childWidth divWidth">
                     <img src="${channel.image}" alt="${channel.name}" />
                 </div>
-                <div > 
-                    <audio controls>
+                <div class="flex innerBorder padding"> 
+                    <h2>${channel.name}</h2>
+                    <audio controls class="audio">
                         <source src="${channel.liveaudio.url}" type="audio/mpeg" />
                     </audio>
                 </div>
@@ -35,3 +35,26 @@ fetch("https://api.sr.se/api/v2/channels/?format=json")
     // Handle any errors that occur during the fetch
     console.error("Fetch error:", error);
   });
+
+  const changeColor =(name) =>{
+    switch(name)
+    {
+      case "P1":
+        return "#00FFFF";
+        console.log(1)
+        break;
+      case "P2":
+        return "#D2691E";
+        console.log(2)
+        break;
+      case "P3":
+        return "#8FBC8F";
+        console.log(3)
+        break;
+      default:
+        return "#9932CC";
+        console.log(4)
+        break;
+    }
+    
+  }
